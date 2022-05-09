8145f14e2d11b3a4c946e6d4f426928f6e9e956f
sketles <dan.velasquez@duocuc.cl>
2022-05-09 00:27:42 -0400

    formulario y pagina

---
+++ formulario.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+
+    $("#enviar").click(function(){
+        var nombre = $("#nombre").val();
+        
+        if(nombre==""){
+            alert("Error, Porfavor rellene el campo nombre");
+        }
+    });
+
+    $("#enviar").click(function(){
+        var email = $("#email").val();
+        
+        if(email==""){
+            alert("Error, Porfavor rellene el campo email");
+        }
+    });
+
+
+    $("#enviar").click(function(){
+        var mensaje = $("#mensaje").val();
+
+        if(mensaje.length <= 50){
+            alert("Porfavor escriba un poco mas (10 caracteres)");
+        }
+
+    });
+
+});
