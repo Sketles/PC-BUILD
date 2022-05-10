fda2554be4c82c2369f2c845c2688fdd35a03392
diegopink <101930702+diegopink@users.noreply.github.com>
2022-05-10 02:28:32 -0500

    1 try jquely

---
+++ loads.js
@@ -0,0 +1,11 @@
+$("#boton").click(function() {
+    $("#htmlext").load('quienes-somos.html');
+});
+
+$("#boton").click(function() {
+    $("#htmlext").load('terminos-y-condiciones.html');
+});
+
+$("#boton").click(function() {
+    $("#htmlext").load('politica-de-reembolso.html');
+});
