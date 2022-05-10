f438a7bb07db82a62cc36e973ad9ff7f4350cf50
diegopink <101930702+diegopink@users.noreply.github.com>
2022-05-10 01:10:57 -0400

    save load 2 carrito y pago exitoso

---
+++ loads-2.js
@@ -0,0 +1,7 @@
+$("#document").click(function() {
+    $("#carrito").load('carrito.html');
+});
+
+$("#document").click(function() {
+    $("#pagoaprobado").load('terminos-y-condiciones.html');
+});
