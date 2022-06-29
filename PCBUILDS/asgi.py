e1638d412dc2e03b2da94385dc64ceca28267010
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-06-28 23:39:24 -0400

    Cambio Nombre de la raiz
    Cambio Nombre de la raiz

--- PCBUILDS/asgi.py
+++ PCBUILDS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TestDjango project.
+ASGI config for PCBUILDS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PCBUILDS.settings')
 
 application = get_asgi_application()
--- PCBUILDS/wsgi.py
+++ PCBUILDS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestDjango project.
+WSGI config for PCBUILDS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PCBUILDS.settings')
 
 application = get_wsgi_application()
